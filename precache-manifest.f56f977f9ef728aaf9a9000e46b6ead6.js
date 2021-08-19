2968e8ad7d5aa8928f0c811e7c6595b8ad851683
lawtechg5 <81251137+lawtechg5@users.noreply.github.com>
2021-08-19 23:33:41 +0100

    Updates

---
+++ precache-manifest.f56f977f9ef728aaf9a9000e46b6ead6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a9178d0586d2e365208",
+    "url": "/honest-share/static/css/main.79843435.chunk.css"
+  },
+  {
+    "revision": "8a9178d0586d2e365208",
+    "url": "/honest-share/static/js/main.8a9178d0.chunk.js"
+  },
+  {
+    "revision": "f480ca66fabd8ab687cf",
+    "url": "/honest-share/static/css/1.94340dd0.chunk.css"
+  },
+  {
+    "revision": "f480ca66fabd8ab687cf",
+    "url": "/honest-share/static/js/1.f480ca66.chunk.js"
+  },
+  {
+    "revision": "396796ae52a0d2808f52",
+    "url": "/honest-share/static/js/runtime~main.396796ae.js"
+  },
+  {
+    "revision": "6c9feef545993172633c9063853c88ad",
+    "url": "/honest-share/index.html"
+  }
+];
